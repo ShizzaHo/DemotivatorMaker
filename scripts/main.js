75448db3bdaa7dd8126cfc90f27eb56b6739d611
ShizzaHo <chekan898@gmail.com>
2022-01-30 11:32:32 +0500

    Первый залив в репозиторий

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let dem = new Demotivator();
+selectedCanvas = dem;
+
+window.onload = function() {
+    setDevotivatorSize();
+}
+
+function createDemotivator() {
+    window.location = "defaultdemotivator.html";
+}
